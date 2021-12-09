53266035ba94c535765aafbdc8d706ea3c0c4214
MeGallin <me@garyallin.uk>
2021-12-09 14:06:06 +0000

    reset and added Sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import './App.scss';
 
 const App = () => {
-  return <div className="App">removed</div>;
+  return (
+    <div className="App">
+      <h1>Cleaned Up</h1>
+      <p>
+        The style sheet has been reset and the all the default colours have been
+        re-set.
+      </p>
+      <p>
+        The style sheet has been reset and the all the default colours have been
+        re-set.
+      </p>
+    </div>
+  );
 };
 
 export default App;
