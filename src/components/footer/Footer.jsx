8f0b3302d632a2f7c191635f8aca6627cc44590c
MeGallin <me@garyallin.uk>
2021-12-10 14:54:00 +0000

    date time comp

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Footer.scss';
+import DateTime from '../dateTime/DateTime';
 
 const Footer = () => {
-  return <footer className="footer">This is the Footer</footer>;
+  return (
+    <footer className="footer">
+      <DateTime />
+    </footer>
+  );
 };
 
 export default Footer;
