dc6e6743631691955fdc7fc4c41c5efe37d0fae1
MeGallin <me@garyallin.uk>
2021-12-10 11:07:19 +0000

    Button comp working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.scss';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import HomeView from './views/HomeView/HomeView';
 
 const App = () => {
   return (
     <>
       <Header />
       <div className="content-wrapper">
-        <h1>Cleaned Up</h1>
-        <p>
-          The style sheet has been reset and the all the default colours have
-          been re-set.
-        </p>
-        <p>
-          The style sheet has been reset and the all the default colours have
-          been re-set.
-        </p>
+        <HomeView />
       </div>
       <Footer />
     </>
---
+++ src/views/HomeView/HomeView.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HomeView.scss';
+
+const HomeView = () => {
+  return <div className="home-view">Home view</div>;
+};
+
+export default HomeView;
