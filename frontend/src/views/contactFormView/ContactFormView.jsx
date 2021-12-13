4bf85a8b76233d87ad6fb756db82b28b4558c29e
MeGallin <me@garyallin.uk>
2021-12-13 19:10:40 +0000

    loader working

--- frontend/src/views/contactFormView/ContactFormView.jsx
+++ frontend/src/views/contactFormView/ContactFormView.jsx
@@ -4,6 +4,7 @@ import { contactFormMessageAction } from '../../store/actions/contactFormActions
 import './ContactFormView.scss';
 import InputField from '../../components/inputField/InputField';
 import Button from '../../components/button/Button';
+import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner';
 
 const ContactFormView = ({ type }) => {
   const dispatch = useDispatch();
@@ -25,41 +26,45 @@ const ContactFormView = ({ type }) => {
 
   return (
     <>
-      <div className="contact-form-view-wrapper">
-        <form>
-          <InputField
-            label="Name"
-            type={type}
-            name={name}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <InputField
-            label="Email"
-            type="email"
-            name={email}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="contact-form-view-wrapper">
+          <form>
+            <InputField
+              label="Name"
+              type={type}
+              name={name}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <InputField
+              label="Email"
+              type="email"
+              name={email}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-          <div>
-            <label htmlFor="message">
-              Message
-              <textarea
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                name="message"
-              />
-            </label>
-          </div>
-          <Button
-            colour="transparent"
-            text="submit"
-            className="btn"
-            onClick={handleSubmit}
-          ></Button>
-        </form>
-      </div>
+            <div>
+              <label htmlFor="message">
+                Message
+                <textarea
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  name="message"
+                />
+              </label>
+            </div>
+            <Button
+              colour="transparent"
+              text="submit"
+              className="btn"
+              onClick={handleSubmit}
+            ></Button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
