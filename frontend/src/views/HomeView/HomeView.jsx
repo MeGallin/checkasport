4ae29eed7c0d714411af51de02c07de1bc0a1811
MeGallin <me@garyallin.uk>
2021-12-16 14:16:27 +0000

    Redux sevices

--- frontend/src/views/HomeView/HomeView.jsx
+++ frontend/src/views/HomeView/HomeView.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listServicesAction } from '../../store/actions/servicesActions';
 import './HomeView.scss';
 
+import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner';
+import Message from '../../components/message/Message';
+
 const HomeView = () => {
-  return <div className="home-view">Home view</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listServicesAction());
+  }, [dispatch]);
+
+  const servicesList = useSelector((state) => state.servicesList);
+  const { loading, success, error, services } = servicesList;
+
+  const closeMessageHandler = () => {
+    dispatch(listServicesAction());
+  };
+
+  return (
+    <>
+      {error ? <Message message={error} onClick={closeMessageHandler} /> : null}
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="home-view">
+          <h1>Services</h1>
+          {services?.map((service) => (
+            <>
+              <div>{service.name}</div>
+            </>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default HomeView;
