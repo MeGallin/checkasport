8175d640d5dd3e50418b293ff8242f1be92dda80
MeGallin <me@garyallin.uk>
2021-12-24 14:31:17 +0000

    Edit and header

--- frontend/src/views/loginFormView/LoginFormView.jsx
+++ frontend/src/views/loginFormView/LoginFormView.jsx
@@ -25,7 +25,7 @@ const LoginFormView = () => {
 
   useEffect(() => {
     if (userInfo && userInfo !== undefined) {
-      navigate('/');
+      navigate('/profile');
     }
   }, [userInfo, navigate]);
 
