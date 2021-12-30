7a36f3f8abac934b321d5f67e89a321b4525c52e
MeGallin <me@garyallin.uk>
2021-12-30 19:37:07 +0000

    Create and Edit profiles

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -2,11 +2,18 @@ import express from 'express';
 import {
   getAllProfiles,
   getProfileById,
+  createProfile,
+  updateProfile,
 } from '../controllers/profileController.js';
+
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/api/profiles').get(getAllProfiles);
-router.route('/api/profile/:id').get(getProfileById);
+router.route('/api/profiles').get(getAllProfiles).post(protect, createProfile);
+router
+  .route('/api/profile/:id')
+  .get(getProfileById)
+  .put(protect, updateProfile);
 
 export default router;
