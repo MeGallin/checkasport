5aa0797e91239c846c7a931e3d77d7cbf0349df9
MeGallin <me@garyallin.uk>
2021-12-30 11:00:24 +0000

    profiles back end

---
+++ backend/controllers/profileController.js
@@ -0,0 +1,17 @@
+import asyncHandler from 'express-async-handler';
+import Profile from '../models/profileModel.js';
+
+// @description: Get All the Profiles
+// @route: GET /api/profiles
+// @access: Public
+const getAllProfiles = asyncHandler(async (req, res) => {
+  const profiles = await Profile.find({});
+  if (profiles) {
+    res.json(profiles);
+  } else {
+    res.status(404);
+    throw new Error('No profiles found');
+  }
+});
+
+export { getAllProfiles };
