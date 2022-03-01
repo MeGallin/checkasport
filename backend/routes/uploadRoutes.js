a678286931ca934a606d38e3074f3dbe5af06422
MeGallin <me@garyallin.uk>
2022-03-01 21:53:10 +0000

    updated routes

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'frontend/public/uploads/profiles');
+    cb(null, 'frontend/build/uploads/profiles');
   },
   filename(req, file, cb) {
     cb(
